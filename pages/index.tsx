db6fc199c6cea13495b5c7215bc7fb7f3077e64b
Samuel Kow Paintsil <smashbros54@gmail.com>
2022-11-12 12:14:44 +0000

    ✨ saturday

--- pages/index.tsx
+++ pages/index.tsx
@@ -34,7 +34,7 @@ export default function Home({
 	return (
 		<>
 			<Head>
-				<title>Samuel Kow Paintsil</title>
+				<title>{pageInfo}</title>
 			</Head>
 			<div className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#329F5B]/80">
 				<Header socials={socials} />
