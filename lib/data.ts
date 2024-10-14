87495d27edc7be0c06d10908c66331c6e69ad2ba
Samuel Kow Paintsil <smashbros54@gmail.com>
2024-10-14 13:01:46 +0000

    🎨 add some projects

--- lib/data.ts
+++ lib/data.ts
@@ -32,9 +32,52 @@ export const BtnList = [
 export const projectsData = [
 	{
 		id: 1,
-		name: 'Dummy Project',
-		description: 'Dummy test project',
-		date: '2022-08-15',
-		demoLink: 'https://google.com',
+		name: 'PDF Whisperer',
+		description:
+			'A tool to give you insights into your PDFs and help you make better decisions.',
+		date: '2024-08-15',
+		demoLink: 'https://pdf-whisperer-drab.vercel.app/',
 	},
+	{
+		id: 2,
+		name: 'Childhood Cancer Society of Ghana Website/Donation Platform',
+		description:
+			'A landing page for the Childhood Cancer Society, a non-profit organization that raises awareness and funds for families affected by childhood cancer.',
+		date: '2024-08-15',
+		demoLink: 'https://childhoodcancersocietygh.com/',
+	},
+	{
+		id: 3,
+		name: 'StackFlow - A stack overflow look-alike',
+		description:
+			'A platform for developers to ask questions and get answers from other developers.Works like stack overflow. PS: Still in development',
+		date: '2024-06-15',
+		demoLink: 'https://stack-flow-nine.vercel.app/',
+	},
+	{
+		id: 4,
+		name: 'Amazon Store Clone',
+		description:
+			'A amazon store clone with just basic amazon cart functionality.',
+		date: '2023-06-15',
+		demoLink: 'https://amazon-2-0-ten.vercel.app/',
+	},
+	{
+		id: 5,
+		name: 'Flip Delivery',
+		description:
+			'A parcel & food delivery app that connects customers to restaurants and delivery riders.',
+		date: '2022-06-15',
+		demoLink:
+			'https://play.google.com/store/search?q=flip%20delivery&c=apps&hl=en_419',
+	},
+	// {
+	// 	id: 6,
+	// 	name: 'Slack Clone',
+	// 	description:
+	// 		'A slack clone with slack like functionality. Chat with your friends and colleagues. PS: Still in development',
+	// 	date: '2022-06-15',
+	// 	demoLink:
+	// 		'https://play.google.com/store/search?q=flip%20delivery&c=apps&hl=en_419',
+	// },
 ]
